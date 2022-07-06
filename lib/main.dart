d0486ac26a7451491d14bfe44defc04780e47f69
Messi002 <rostandmessi2@gmail.com>
2022-07-06 18:06:34 +0100

    generateWords

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
-import 'dart:async';
-
 
 void main() {
   runApp(const MyApp());
@@ -20,59 +18,7 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('English Words'),),
-      body: randomWords(),
-    );
-  }
-}
-
-
-
-
-class _randomWordsState extends State<randomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = <WordPair>{};
-  final _biggerFont = const TextStyle(fontSize: 18);
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        // itemCount: _suggestions.length,
-        itemBuilder: (context, i){
-          // _suggestions.addAll(generateWordPairs().take(10));
-          if(i.isOdd) return const Divider();
-          final index = i ~/ 2;
-          if(index >= _suggestions.length){
-                _suggestions.addAll(generateWordPairs().take(10));
-          }
-            final alreadySaved = _saved.contains(_suggestions[index]);
-          return ListTile(
-            title:  Text(_suggestions[index].asPascalCase, style: _biggerFont,),
-            trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
-            color: alreadySaved? Colors.red: null,
-            semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
-            ),
-            onTap:(){
-                setState(() {
-                  if(alreadySaved) {_saved.remove(_suggestions[index]);}
-                  else{
-                          _saved.add(_suggestions[index]);
-                  }
-                });
-            },
-          );
-        }
+      home: const randomWords(),
     );
   }
 }
@@ -80,9 +26,93 @@ class _randomWordsState extends State<randomWords> {
 class randomWords extends StatefulWidget {
   const randomWords({Key? key}) : super(key: key);
 
+
+
+
   @override
   State<randomWords> createState() => _randomWordsState();
 }
 
+class _randomWordsState extends State<randomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
+  final _biggerFont = const TextStyle(fontSize: 18);
 
+  void _pushSaved() {
+    Navigator.of(context).push(
+      // Add lines from here...
+      MaterialPageRoute<void>(
+        builder: (context) {
+          final tiles = _saved.map(
+            (pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(
+                  context: context,
+                  tiles: tiles,
+                ).toList()
+              : <Widget>[];
 
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ), // ...to here.
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('English Words'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.list),
+            onPressed: _pushSaved,
+            tooltip: 'Saved Suggestions',
+          ),
+        ],
+      ),
+      body: ListView.builder(
+          padding: const EdgeInsets.all(16.0),
+          itemBuilder: (_, i) {
+            if (i.isOdd) return const Divider();
+            final index = i ~/ 2;
+            if (index >= _suggestions.length) {
+              _suggestions.addAll(generateWordPairs().take(10));
+            }
+            final alreadySaved = _saved.contains(_suggestions[index]);
+            return ListTile(
+              title: Text(
+                _suggestions[index].asPascalCase,
+                style: _biggerFont,
+              ),
+              trailing: Icon(
+                alreadySaved ? Icons.favorite : Icons.favorite_border,
+                color: alreadySaved ? Colors.red : null,
+                semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+              ),
+              onTap: () {
+                setState(() {
+                  if (alreadySaved) {
+                    _saved.remove(_suggestions[index]);
+                  } else {
+                    _saved.add(_suggestions[index]);
+                  }
+                });
+              },
+            );
+          }),
+    );
+  }
+}
